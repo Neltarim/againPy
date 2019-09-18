d380e7a136d668a9f7c843875d8151cfe0eadcd1
Neltarim <kyurann@gmail.com>
2019-09-18 04:06:48 +0200

    new aliases

--- new-machine/code_aliases.zsh
+++ new-machine/code_aliases.zsh
@@ -1,28 +1,40 @@
-#alias for alias (lol)
+#alias
 alias aliasedit='vi ~/.oh-my-zsh/custom/code_aliases.zsh'
 alias aliasupdate='source ~/.zshrc'
+alias aliasup='source ~/.zshrc'
 
-#alias for running python 3 script
+#python
 alias py='python3'
+alias py2='python2'
+alias supy='sudo python3'
 
-
-#aliases for virtualenv
+#virtualenv
 alias virtualnew='virtualenv -p python3 env'
 alias virtualstart='source env/bin/activate'
 alias virtualstop='deactivate'
 alias virtualdelete='rm -rf ./env/'
 
-#hub aliases
+#hub
+alias hubinit='hub init && hub create'
 alias hubadd='git add *'
 alias hubcommit='git commit -m'
 alias hubpush='git push origin master'
 
-#aliases for starting VS code
+#VS code
 alias vs='code .'
 alias vsdir='code ..'
 
-#alias for againPy
+#againPy
 alias again='python3 ~/Documents/againPy/again.py'
 
-#alias for zsh
+#zsh
+alias rm='rm -r'
 alias sudorm='sudo rm -rf'
+alias mk='mkdir'
+
+#pip
+alias pip='sudo pip3'
+alias freeze='pip3 freeze > requirements.txt'
+
+#django
+alias djangonew='django-admin startproject'
